cbf612033e58f089eaa15cdf81564b5afa02d555
Wasi Ur Rahman <48942495+Wasi-Ur-Rahman@users.noreply.github.com>
2024-01-09 13:28:27 +0600

    Update index.js

--- js/index.js
+++ js/index.js
@@ -24,9 +24,6 @@ function showSongs(data) {
         songDiv.innerHTML = `<div class="col-md-9">
         <h3 class="lyrics-name">${title}</h3>
         <p class="author lead">Album by <span>${artist}</span></p>
-        <audio controls>
-            <source src="${preview}" type="audio/mpeg">
-        </audio>
         </div>
         <div class="col-md-3 text-md-right text-center">
             <button onclick="getLyrics('${artist}','${title}')" class="btn btn-success">Get Lyrics</button>
